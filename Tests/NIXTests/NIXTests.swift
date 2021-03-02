9c2b24385f8a1d434c0d589f579f7b8d5f8f2601
Chip Jarred <chipjarred@gmail.com>
2021-03-02 06:27:22 -0600

    Created unit tests for IPv4, IPv6 and UNIX domain sockets; however, the IPv4 test doesn't work because of entitlements with XCTest runner.  It's a documented issue with Apple.  For some reason the iPv6 does run, which seems like a bug, but it did at least allow me to confirm that the test passes.

--- Tests/NIXTests/NIXTests.swift
+++ Tests/NIXTests/NIXTests.swift
@@ -1,11 +1,273 @@
 import XCTest
 @testable import NIX
 
+// -------------------------------------
 final class NIXTests: XCTestCase
 {
-    func testExample() {  }
-
-    static var allTests = [
-        ("testExample", testExample),
+    static var allTests =
+    [
+        ("test_ip4_client_can_connect_to_server", test_ip4_client_can_connect_to_server),
+        ("test_ip6_client_can_connect_to_server", test_ip6_client_can_connect_to_server),
+        ("test_unix_domain_client_can_connect_to_server", test_unix_domain_client_can_connect_to_server),
     ]
+    
+    // -------------------------------------
+    /*
+     WARNING: This test fails due to entitlements issue with XCTest runner on
+     macOS. I have it disabled in Xcode.  In order to successfully test it, you
+     have to run it in an app with the appropriate entitlements.
+     
+     see: https://developer.apple.com/forums/thread/114907
+
+     I *think* it will work on Linux where Apple entitlements isn't an issue,
+     so long as the user running XCTest has permissions to bind to ports, but I
+     don't have Linux installed anywhere to try it.
+     
+     The server socket fails on the call to NIX.bind.
+     
+     Strangely, the IPv6 test works, which seems like a bug in entitlements.
+     Either it's a bug because IP4 should work too, or it's a bug because the
+     entitlements don't protect against IPv6 network access.  Either way it's a
+     bug.
+     
+     */
+    func test_ip4_client_can_connect_to_server()
+    {
+        let serverPort = 2020
+        
+        let clientToServerMessage =
+            "This is a test of the Emergency Broadcast System"
+        let serverToClientMessage =
+            "Had this been a real emergency, this message would not help at all"
+        
+        var messageServerReceived = ""
+        let server = IP4TestStreamServer(port: serverPort).onMessageReceipt
+        { clientMessage in
+            messageServerReceived =
+                String(data: clientMessage, encoding: .utf8)!
+            return serverToClientMessage.data(using: .utf8)!
+        }
+        
+        server.start()
+        
+        defer { server.terminate() }
+        
+        let socket: SocketIODescriptor
+        switch NIX.socket(.inet4, .stream, .ip)
+        {
+            case .success(let s): socket = s
+            case .failure(let e):
+                XCTFail("Failed to create socket \(e)")
+                return
+        }
+        
+        defer
+        {
+            if let error = NIX.close(socket) {
+                XCTFail("Failed to close client socket: \(error)")
+            }
+        }
+        
+        let serverAddress = NIX.socketAddress(for: .loopback, port: serverPort)
+        
+        if let error = NIX.connect(socket, serverAddress)
+        {
+            XCTFail("Failed to connect to server: \(error)")
+            return
+        }
+        
+        switch NIX.write(socket, clientToServerMessage.data(using: .utf8)!)
+        {
+            case .success(let bytesWritten):
+                XCTAssertEqual(bytesWritten, clientToServerMessage.count)
+            case .failure(let e):
+                XCTFail("Client write failed: \(e)")
+                return
+        }
+        
+        var readBuffer = Data(repeating: 0, count: 1024)
+        switch NIX.read(socket, &readBuffer)
+        {
+            case .success(let bytesRead):
+                XCTAssertEqual(bytesRead, serverToClientMessage.count)
+                readBuffer.removeLast(readBuffer.count - bytesRead)
+            
+            case .failure(let e):
+                XCTFail("Client write failed: \(e)")
+                return
+        }
+        
+        XCTAssertEqual(clientToServerMessage, messageServerReceived)
+        
+        let messageClientReceived = String(data: readBuffer, encoding: .utf8)!
+        XCTAssertEqual(serverToClientMessage, messageClientReceived)
+    }
+    
+    // -------------------------------------
+    func test_ip6_client_can_connect_to_server()
+    {
+        let serverPort = 2021
+        
+        let clientToServerMessage =
+            "This is a test of the Emergency Broadcast System"
+        let serverToClientMessage =
+            "Had this been a real emergency, this message would not help at all"
+        
+        var messageServerReceived = ""
+        let server = IP6TestStreamServer(port: serverPort).onMessageReceipt
+        { clientMessage in
+            messageServerReceived =
+                String(data: clientMessage, encoding: .utf8)!
+            return serverToClientMessage.data(using: .utf8)!
+        }
+        
+        server.start()
+        
+        defer { server.terminate() }
+        
+        let socket: SocketIODescriptor
+        switch NIX.socket(.inet6, .stream, .ip)
+        {
+            case .success(let s): socket = s
+            case .failure(let e):
+                XCTFail("Failed to create socket \(e)")
+                return
+        }
+        
+        defer
+        {
+            if let error = NIX.close(socket) {
+                XCTFail("Failed to close client socket: \(error)")
+            }
+        }
+        
+        let serverAddress = NIX.socketAddress(for: .loopback, port: serverPort)
+        
+        if let error = NIX.connect(socket, serverAddress)
+        {
+            XCTFail("Failed to connect to server: \(error)")
+            return
+        }
+        
+        switch NIX.write(socket, clientToServerMessage.data(using: .utf8)!)
+        {
+            case .success(let bytesWritten):
+                XCTAssertEqual(bytesWritten, clientToServerMessage.count)
+            case .failure(let e):
+                XCTFail("Client write failed: \(e)")
+                return
+        }
+        
+        var readBuffer = Data(repeating: 0, count: 1024)
+        switch NIX.read(socket, &readBuffer)
+        {
+            case .success(let bytesRead):
+                XCTAssertEqual(bytesRead, serverToClientMessage.count)
+                readBuffer.removeLast(readBuffer.count - bytesRead)
+            
+            case .failure(let e):
+                XCTFail("Client write failed: \(e)")
+                return
+        }
+        
+        XCTAssertEqual(clientToServerMessage, messageServerReceived)
+        
+        let messageClientReceived = String(data: readBuffer, encoding: .utf8)!
+        XCTAssertEqual(serverToClientMessage, messageClientReceived)
+    }
+    
+    // -------------------------------------
+    func test_unix_domain_client_can_connect_to_server()
+    {
+        let serverPath =
+            "\(ProcessInfo.processInfo.processIdentifier).serverSocket"
+        
+        if let error = NIX.unlink(serverPath), error.errno != HostOS.ENOENT
+        {
+            XCTFail("Failed to remove previous socket file")
+            return
+        }
+        
+        let clientToServerMessage =
+            "This is a test of the Emergency Broadcast System"
+        let serverToClientMessage =
+            "Had this been a real emergency, this message would not help at all"
+        
+        var messageServerReceived = ""
+        let maybeServer = UnixDomainTestStreamServer(path: serverPath)?
+            .onMessageReceipt
+            { clientMessage in
+                messageServerReceived =
+                    String(data: clientMessage, encoding: .utf8)!
+                return serverToClientMessage.data(using: .utf8)!
+            }
+        
+        guard let server = maybeServer else
+        {
+            XCTFail("Failed to create UnixDomainTestStreamServer")
+            return
+        }
+        
+        server.start()
+        
+        defer { server.terminate() }
+        
+        let socket: SocketIODescriptor
+        switch NIX.socket(.local, .stream, .ip)
+        {
+            case .success(let s): socket = s
+            case .failure(let e):
+                XCTFail("Failed to create socket \(e)")
+                return
+        }
+        
+        defer
+        {
+            if let error = NIX.close(socket) {
+                XCTFail("Failed to close client socket: \(error)")
+            }
+        }
+        
+        guard let unixPath = NIX.UnixSocketPath(serverPath) else
+        {
+            XCTFail(
+                "Failed to create UnixSocketPath for path = \"\(serverPath)\""
+            )
+            return
+        }
+        
+        let serverAddress = NIX.socketAddress(for: unixPath)
+        
+        if let error = NIX.connect(socket, serverAddress)
+        {
+            XCTFail("Failed to connect to server: \(error)")
+            return
+        }
+        
+        switch NIX.write(socket, clientToServerMessage.data(using: .utf8)!)
+        {
+            case .success(let bytesWritten):
+                XCTAssertEqual(bytesWritten, clientToServerMessage.count)
+            case .failure(let e):
+                XCTFail("Client write failed: \(e)")
+                return
+        }
+        
+        var readBuffer = Data(repeating: 0, count: 1024)
+        switch NIX.read(socket, &readBuffer)
+        {
+            case .success(let bytesRead):
+                XCTAssertEqual(bytesRead, serverToClientMessage.count)
+                readBuffer.removeLast(readBuffer.count - bytesRead)
+            
+            case .failure(let e):
+                XCTFail("Client write failed: \(e)")
+                return
+        }
+        
+        XCTAssertEqual(clientToServerMessage, messageServerReceived)
+        
+        let messageClientReceived = String(data: readBuffer, encoding: .utf8)!
+        XCTAssertEqual(serverToClientMessage, messageClientReceived)
+    }
 }
---
+++ Tests/NIXTests/TestStreamServers.swift
@@ -0,0 +1,343 @@
+// Copyright 2021 Chip Jarred
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+import Dispatch
+import XCTest
+import NIX
+
+// -------------------------------------
+extension DispatchSemaphore
+{
+    func withLock<R>(_ block: () throws -> R) rethrows -> R
+    {
+        wait()
+        defer { signal() }
+        return try block()
+    }
+}
+
+// MARK:-
+// -------------------------------------
+class StreamServer<SockAddressType: SocketAddress>
+{
+    typealias SockAddressType = SockAddressType
+    let uuid = UUID()
+    
+    let serverQueue: DispatchQueue
+    let clientQueue: DispatchQueue
+    private(set) var messageReceiptHandler: ((Data) -> Data?)? = nil
+    
+    let termMutex = DispatchSemaphore(value: 1)
+    private var _shouldTerminate = false
+    private var shouldTerminate: Bool
+    {
+        get { return termMutex.withLock { _shouldTerminate } }
+        set { termMutex.withLock { _shouldTerminate = newValue } }
+    }
+    
+    // -------------------------------------
+    init()
+    {
+        self.serverQueue = DispatchQueue(
+            label: "server-\(uuid)",
+            attributes: .concurrent
+        )
+        self.clientQueue = DispatchQueue(
+            label: "client-\(uuid)",
+            attributes: .concurrent
+        )
+    }
+    
+    // -------------------------------------
+    func onMessageReceipt(do handler: @escaping (Data) -> Data?) -> Self
+    {
+        self.messageReceiptHandler = handler
+        return self
+    }
+    
+    // -------------------------------------
+    func terminate()
+    {
+        shouldTerminate = true
+        connectAndClose()
+    }
+    
+    // -------------------------------------
+    func makeSocket() -> Result<SocketIODescriptor, NIX.Error> {
+        fatalError("Override me!: \(#function)")
+    }
+    
+    // -------------------------------------
+    func makeServerSocketAddressForClient() -> SockAddressType {
+        fatalError("Override me!: \(#function)")
+    }
+    
+    // -------------------------------------
+    func makeServerSocketAddressForServer() -> SockAddressType {
+        fatalError("Override me!: \(#function)")
+    }
+    
+    // -------------------------------------
+    private func connectAndClose()
+    {
+        let socket: SocketIODescriptor
+        switch makeSocket()
+        {
+            case .success(let s): socket = s
+            case .failure(let error): fatalError("\(error)")
+        }
+        
+        let socketAddress = makeServerSocketAddressForClient()
+        if let error = NIX.connect(socket, socketAddress) {
+            fatalError("\(error)")
+        }
+        
+        _ = NIX.close(socket)
+    }
+
+    // -------------------------------------
+    func start()
+    {
+        let listenSocket: SocketIODescriptor
+        switch makeSocket()
+        {
+            case .success(let sock): listenSocket = sock
+            case .failure(let error):
+                fatalError("Could not create listener socket: \(error)")
+        }
+
+        let socketAddress = makeServerSocketAddressForServer()
+
+        if let error = NIX.bind(listenSocket, socketAddress)
+        {
+            _ = close(listenSocket)
+            fatalError("Could not bind listener socket: \(error)")
+        }
+
+        if let error = NIX.listen(listenSocket, 100)
+        {
+            _ = close(listenSocket)
+            fatalError("Could not listen on listener socket: \(error)")
+        }
+        
+        let waitSem = DispatchSemaphore(value: 0)
+
+        serverQueue.async
+        {
+            defer { _ = NIX.close(listenSocket) }
+            
+            waitSem.signal()
+            
+            print("Server started...")
+
+            while true
+            {
+                var peerAddress = HostOS.sockaddr_in6()
+                var peerSocket: SocketIODescriptor
+                switch NIX.accept(listenSocket, &peerAddress)
+                {
+                    case .success(let sock): peerSocket = sock
+                    case .failure(let error):
+                        fatalError("Accept failed on listener socket: \(error)")
+                }
+                
+                if self.shouldTerminate
+                {
+                    print("Server terminating")
+                    _ = NIX.close(peerSocket)
+                    break
+                }
+                
+                self.clientQueue.async
+                {
+                    self.handlePeerSession(
+                        with: peerSocket,
+                        peerAddress: peerAddress
+                    )
+                }
+            }
+        }
+        
+        waitSem.wait() // Wait until server loop has started before returning
+        waitSem.signal()
+    }
+
+    // -------------------------------------
+    private func handlePeerSession(
+        with peerSocket: SocketIODescriptor,
+        peerAddress: sockaddr_in6)
+    {
+        defer { _ = NIX.close(peerSocket) }
+        
+        var readBuffer = Data(repeating: 0, count: 1024 * 4)
+
+        clientLoop: while true
+        {
+            // Read data from client
+            var peerMessage: Data
+            switch NIX.read(peerSocket, &readBuffer)
+            {
+                case .success(let bytesRead):
+                    if bytesRead == 0 {
+                        print("Peer closed connection")
+                        break clientLoop
+                    }
+                    
+                    peerMessage = readBuffer.withUnsafeMutableBytes
+                    {
+                        Data(
+                            bytesNoCopy: $0.baseAddress!,
+                            count: bytesRead,
+                            deallocator: .none
+                        )
+                    }
+                    break
+                    
+                case .failure(let error):
+                    if error.errno == HostOS.EAGAIN { continue }
+                    print("Error reading from peer socket: \(error)")
+                    break clientLoop
+            }
+            
+            guard let response = responseFor(peerMessage) else { continue }
+            
+            
+            // Write response to client
+            switch NIX.write(peerSocket, response)
+            {
+                case .success(let bytesWritten):
+                    if bytesWritten != response.count {
+                        print("Not all bytes were written to peer socket")
+                    }
+                case .failure(let error):
+                    print("Error writing to peer socket: \(error)")
+                    break clientLoop
+            }
+        }
+    }
+    
+    // -------------------------------------
+    func responseFor(_ clientData: Data) -> Data?
+    {
+        guard let handler = messageReceiptHandler else { return nil }
+        
+        return handler(clientData)
+    }
+}
+
+// MARK:-
+// -------------------------------------
+class IP4TestStreamServer: StreamServer<HostOS.sockaddr_in>
+{
+    let address: HostOS.in_addr
+    let port: Int
+    
+    // -------------------------------------
+    init(address: HostOS.in_addr = .any, port: Int)
+    {
+        self.address = address
+        self.port = port
+    }
+    
+    // -------------------------------------
+    override func makeSocket() -> Result<SocketIODescriptor, NIX.Error> {
+        return NIX.socket(.inet4, .stream, .ip)
+    }
+    
+    // -------------------------------------
+    override func makeServerSocketAddressForClient() -> sockaddr_in
+    {
+        return NIX.socketAddress(
+            for: .loopback,
+            port: port,
+            family: .inet4
+        ) as! sockaddr_in
+    }
+    
+    // -------------------------------------
+    override func makeServerSocketAddressForServer() -> sockaddr_in
+    {
+        return NIX.socketAddress(
+            for: .any,
+            port: port,
+            family: .inet4
+        ) as! sockaddr_in
+    }
+}
+
+// MARK:-
+// -------------------------------------
+class IP6TestStreamServer: StreamServer<HostOS.sockaddr_in6>
+{
+    let address: HostOS.in6_addr
+    let port: Int
+    
+    // -------------------------------------
+    init(address: HostOS.in6_addr = .any, port: Int)
+    {
+        self.address = address
+        self.port = port
+    }
+    
+    // -------------------------------------
+    override func makeSocket() -> Result<SocketIODescriptor, NIX.Error> {
+        return NIX.socket(.inet6, .stream, .ip)
+    }
+    
+    // -------------------------------------
+    override func makeServerSocketAddressForClient() -> sockaddr_in6 {
+        return NIX.socketAddress(for: .loopback, port: port) as! sockaddr_in6
+    }
+    
+    // -------------------------------------
+    override func makeServerSocketAddressForServer() -> sockaddr_in6 {
+        return NIX.socketAddress(for: .any, port: port) as! sockaddr_in6
+    }
+}
+
+// MARK:-
+// -------------------------------------
+class UnixDomainTestStreamServer: StreamServer<HostOS.sockaddr_un>
+{
+    let path: NIX.UnixSocketPath
+    
+    // -------------------------------------
+    init?(path: String = "\(ProcessInfo.processInfo.processIdentifier).socket")
+    {
+        guard let unixPath = UnixSocketPath(path) else { return nil }
+        self.path = unixPath
+    }
+    
+    // -------------------------------------
+    override func makeSocket() -> Result<SocketIODescriptor, NIX.Error> {
+        return NIX.socket(.local, .stream, .ip)
+    }
+    
+    // -------------------------------------
+    override func makeServerSocketAddressForClient() -> sockaddr_un {
+        return NIX.socketAddress(for: path) as! sockaddr_un
+    }
+    
+    // -------------------------------------
+    override func makeServerSocketAddressForServer() -> sockaddr_un {
+        return NIX.socketAddress(for: path) as! sockaddr_un
+    }
+}
