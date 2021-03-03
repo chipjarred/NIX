2d9e0032b066b01a53f2144b7792707424a08eca
Chip Jarred <chipjarred@gmail.com>
2021-03-02 23:00:43 -0600

    Updating XCTestManifests to include the datagram socket tests.

--- Tests/NIXTests/XCTestManifests.swift
+++ Tests/NIXTests/XCTestManifests.swift
@@ -4,6 +4,7 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(StreamSocket_UnitTests.allTests),
+        testCase(DatagramSocket_UnitTests.allTests),
     ]
 }
 #endif
