f0a478917b608bed1fed76f24866f0417289242c
Chip Jarred <chipjarred@gmail.com>
2021-03-03 18:38:45 -0600

    Added unit tests for converting binary IPv4 and IPv6 address to strings

---
+++ Tests/NIXTests/HostOSAddress_UnitTests.swift
@@ -0,0 +1,180 @@
+import XCTest
+@testable import NIX
+
+// -------------------------------------
+class HostOSAddress_UnitTests: XCTestCase
+{
+    static var allTests =
+    [
+        ("test_IP4_description_is_correct", test_IP4_description_is_correct),
+        ("test_IP6_description_is_correct", test_IP6_description_is_correct),
+    ]
+    
+    // -------------------------------------
+    func test_IP4_description_is_correct()
+    {
+        let testCases: [(input: in_addr_t, expected: String)] =
+        [
+            (input: 0x00000000, expected: "0.0.0.0"),
+            (input: 0x00000001, expected: "0.0.0.1"),
+            (input: 0x00000100, expected: "0.0.1.0"),
+            (input: 0x00010000, expected: "0.1.0.0"),
+            (input: 0x01000000, expected: "1.0.0.0"),
+            (input: 0x7F000001, expected: "127.0.0.1"),
+            (input: 0xFFFFFFFF, expected: "255.255.255.255"),
+            (input: 0x1F0110F1, expected: "31.1.16.241"),
+        ]
+        for (input, expected) in testCases
+        {
+            let address = in_addr(s_addr: input.toNetworkByteOrder)
+            
+            let actual = address.description
+            XCTAssertEqual(actual, expected)
+        }
+    }
+    
+    // -------------------------------------
+    func test_IP6_description_is_correct()
+    {
+        typealias BinaryAddress = (
+            __uint16_t, __uint16_t, __uint16_t, __uint16_t,
+            __uint16_t, __uint16_t, __uint16_t, __uint16_t
+        )
+        
+        // IPv6 addresses are compressed when rendered in string format
+        // So ensure that rules about leading zeros and zero fields are
+        // followed.
+        let testCases: [(input:BinaryAddress, expected: String)] =
+        [
+            (   // 0000:0000:0000:0000:0000:0000:0000:0000
+                input: (
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder
+                ),
+                expected: "::"
+            ),
+            (   // 0000:0000:0000:0000:0000:0000:0000:0001
+                input: (
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0001).toNetworkByteOrder
+                ),
+                expected: "::1"
+            ),
+            (   // 0000:0000:0000:0000:0000:0000:0000:0001
+                input: (
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x1000).toNetworkByteOrder
+                ),
+                expected: "::1000"
+            ),
+            (   // 0000:0000:0000:0000:0000:0000:0000:0000
+                input: (
+                    __uint16_t(0x0001).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder
+                ),
+                expected: "1::"
+            ),
+            (   // 0000:0000:0000:0000:0000:0000:0000:0000
+                input: (
+                    __uint16_t(0x1000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder
+                ),
+                expected: "1000::"
+            ),
+            (   // 0000:0000:0000:0000:0000:8a2e:0370:7334
+                input: (
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x8a2e).toNetworkByteOrder,
+                    __uint16_t(0x0370).toNetworkByteOrder,
+                    __uint16_t(0x7334).toNetworkByteOrder
+                ),
+                expected: "::8a2e:370:7334"
+            ),
+            (   // 2001:0db8:85a3:0000:0000:0000:0000:0000
+                input: (
+                    __uint16_t(0x2001).toNetworkByteOrder,
+                    __uint16_t(0x0db8).toNetworkByteOrder,
+                    __uint16_t(0x85a3).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder
+                ),
+                expected: "2001:db8:85a3::"
+            ),
+            (   // 2001:0db8:85a3:0000:0000:8a2e:0370:7334
+                input: (
+                    __uint16_t(0x2001).toNetworkByteOrder,
+                    __uint16_t(0x0db8).toNetworkByteOrder,
+                    __uint16_t(0x85a3).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x8a2e).toNetworkByteOrder,
+                    __uint16_t(0x0370).toNetworkByteOrder,
+                    __uint16_t(0x7334).toNetworkByteOrder
+                ),
+                expected: "2001:db8:85a3::8a2e:370:7334"
+            ),
+            (   // 2001:0db8:0000:0000:85a3:0000:0000:7334
+                input: (
+                    __uint16_t(0x2001).toNetworkByteOrder,
+                    __uint16_t(0x0db8).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x85a3).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x0000).toNetworkByteOrder,
+                    __uint16_t(0x7334).toNetworkByteOrder
+                ),
+                expected: "2001:db8::85a3:0:0:7334"
+            ),
+        ]
+        
+        for (input, expected) in testCases
+        {
+            let address = in6_addr(
+                __u6_addr: in6_addr.__Unnamed_union___u6_addr.init(
+                    __u6_addr16: input
+                )
+            )
+            
+            let actual = address.description
+            XCTAssertEqual(actual, expected)
+        }
+    }
+}
--- Tests/NIXTests/XCTestManifests.swift
+++ Tests/NIXTests/XCTestManifests.swift
@@ -5,6 +5,7 @@ public func allTests() -> [XCTestCaseEntry]
 {
     return [
         testCase(FileIO_UnitTests.allTests),
+        testCase(HostOSAddress_UnitTests.allTests),
         testCase(StreamSocket_UnitTests.allTests),
         testCase(DatagramSocket_UnitTests.allTests),
     ]
