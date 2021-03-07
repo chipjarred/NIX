a4d68ac1b77e813d9687ec857699a2a7aec789b6
Chip Jarred <chipjarred@gmail.com>
2021-03-07 03:23:00 -0600

    Decided not to re-export HostOS.  After using it that way for a bit, it actually makes NIX harder to use, because even when explicitly specifying the NIX module in names Xcode's autocompletion lists the underlying POSIX API, which is totally the wrong thing.

--- Sources/NIX/NIX.swift
+++ Sources/NIX/NIX.swift
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-@_exported import HostOS
+import HostOS
 
 // -------------------------------------
 /// Wrapper for standard C/Darwin's `errno`
